dbd1ec3bb0e22002016b263b9d0d214131cfb3a5
David A. Martinez Figueroa <damf@ciencias.unam.mx>
2020-05-25 14:37:42 -0500

    Categoria

--- trajinera/apps/Usuario/urls.py
+++ trajinera/apps/Usuario/urls.py
@@ -3,11 +3,22 @@ from apps.Usuario.views import *
 
 urlpatterns = [
     path('index_administrador/', indexAdministrador, name = 'index_menu'),
+    
     path('menu_Alimentos_Administrador/', menu_Alimentos_Administrador.as_view() , name = 'menu_alimentos'),
     path('menu_Alimentos_Editar/', menu_Alimentos_Editar.as_view() , name = 'menu_editar_alimentos'),
     path('menu_Alimentos_Eliminar/', menu_Alimentos_Eliminar.as_view() , name = 'menu_eliminar_alimentos'),
     path('menu_Alimentos_crear/', Crear_Alimento.as_view() , name = 'crear_alimento'),
     path('editar_Alimento/<pk>', Editar_Alimento.as_view() , name = 'editar_alimento'),
     path('eliminar_Alimento/<pk>', Eliminar_Alimento.as_view() , name = 'eliminar_alimento'),
+    
+    path('menu_Categorias/', menu_Categoria.as_view() , name = 'listado_categorias'),
+    path('menu_Categorias_Editar/', menu_Editar_Categoria.as_view() , name = 'listado_editar_categorias'),
+    path('menu_Categorias_Eliminar/', menu_Eliminar_Categoria.as_view() , name = 'listado_eliminar_categorias'),
+    path('menu_Categorias_crear/', Crear_Categoria.as_view() , name = 'crear_categoria'),
+    path('editar_Categoria/<pk>', Editar_Categoria.as_view() , name = 'editar_categoria'),
+    path('eliminar_Categoria/<pk>', Eliminar_Categoria.as_view() , name = 'eliminar_categoria'),
+    path('Categoria_Alimentos/<categoria>', menu_Categoria_Alimentos , name = 'categoria_alimentos'),
+
+
 ]
 
--- trajinera/apps/Usuario/views.py
+++ trajinera/apps/Usuario/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse 
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
-from apps.Menu.models import Alimento
-from apps.Usuario.forms import AlimentoForm
+from apps.Menu.models import Alimento, Categoria 
+from apps.Usuario.forms import AlimentoForm, CategoriaForm
 
 # Create your views here.
 
@@ -25,9 +25,6 @@ class menu_Alimentos_Eliminar(ListView):
 	template_name = 'admin/alimento/menu_eliminar_alimento.html'
 
 
-	
-
-
 class Crear_Alimento(CreateView):
 	model = Alimento
 	form_class = AlimentoForm
@@ -45,6 +42,51 @@ class Eliminar_Alimento(DeleteView):
 	form_class = AlimentoForm
 	template_name = 'admin/alimento/eliminar_alimento.html'
 	success_url = reverse_lazy('menu_alimentos')
+
+
+
+
+class menu_Categoria(ListView):
+	model = Categoria
+	template_name = 'admin/categoria/categorias.html'
+
+
+def menu_Categoria_Alimentos(request, categoria):
+	alimento = Alimento.objects.all()
+	contexto = {'alimentos': alimento, 'Categoria': categoria }
+	return render(request, 'admin/categoria/alimentos_categoria.html', contexto) 
+
+
+class menu_Editar_Categoria(ListView):
+	model = Categoria
+	template_name = 'admin/categoria/menu_editar_categoria.html'
+
+class menu_Eliminar_Categoria(ListView):
+	model = Categoria
+	template_name = 'admin/categoria/menu_eliminar_categoria.html'
+
+
+class Crear_Categoria(CreateView):
+	model = Categoria
+	form_class = CategoriaForm
+	template_name = 'admin/categoria/crear_categoria.html'
+	success_url = reverse_lazy('listado_categorias')
+
+class Editar_Categoria(UpdateView):
+	model = Categoria
+	form_class = CategoriaForm
+	template_name = 'admin/categoria/editar_categoria.html'
+	success_url = reverse_lazy('listado_categorias')
+
+class Eliminar_Categoria(DeleteView):
+	model = Categoria
+	form_class = CategoriaForm
+	template_name = 'admin/categoria/eliminar_categoria.html'
+	success_url = reverse_lazy('listado_categorias')
+
+
+
+
 
 
 
